be2ff0cdec12df197f22bade3e5ee9fd6f7008f0
YinjieXIA <18256095603@163.com>
2023-12-31 19:34:05 +0100

    完善了一下匿名登录部分，确定了首页为group页面

--- app/src/main/java/com/example/pptcomments/MainActivity.kt
+++ app/src/main/java/com/example/pptcomments/MainActivity.kt
@@ -3,7 +3,18 @@ package com.example.pptcomments
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -11,12 +22,37 @@ import androidx.navigation.compose.composable
 import com.example.pptcomments.learningGroup.*
 import com.example.pptcomments.uploadAndShare.SharePPTViewModel
 import com.example.pptcomments.uploadAndShare.UploadPPTScreen
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val auth = Firebase.auth
+
         setContent {
-            MyApp()
+            val isLoggingIn = remember { mutableStateOf(true) }
+            val loginFailed = remember { mutableStateOf(false) }
+
+            LaunchedEffect(Unit) {
+                auth.signInAnonymously().addOnCompleteListener { task ->
+                    isLoggingIn.value = false
+                    loginFailed.value = !task.isSuccessful
+                }
+            }
+
+            if (isLoggingIn.value) {
+                CircularProgressIndicator()
+            } else if (loginFailed.value) {
+                LoginFailedScreen {
+                    isLoggingIn.value = true
+                    loginFailed.value = false
+                }
+            } else {
+                MyApp()
+            }
         }
     }
 }
@@ -46,5 +82,26 @@ fun MyApp() {
             )
         }
         // 添加其他必要的路由
+    }
+}
+
+@Composable
+fun LoadingScreen() {
+    // 显示一个加载动画或信息
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        CircularProgressIndicator()
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Logging in...")
+    }
+}
+
+@Composable
+fun LoginFailedScreen(retryLogin: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Text("Login failed. Please check your network connection.")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = retryLogin) {
+            Text("Retry")
+        }
     }
 }
